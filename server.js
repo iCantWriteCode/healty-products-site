936a5629dd53eac5ac93fd4f1475ecbf3e9994d9
elissaios <elissaios13@hotmail.com>
2018-02-18 13:27:09 +0200

    prerender??

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, ''));
 app.set('view engine', 'ejs');
 
-app.use(require('prerender-node').set('prerenderToken', '1chV2LTbzMvt1S0zuo6y'));
+app.use(()=>{
+	console.log("mpika")
+	require('prerender-node').set('prerenderToken', '1chV2LTbzMvt1S0zuo6y')
+});
 
 app.get('/*', (req, res) => {
 	res.render('index');
