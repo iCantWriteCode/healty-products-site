867de5c4200ab62efc3afad8a135a36227d92f0c
elissaios <elissaios13@hotmail.com>
2018-02-18 12:46:41 +0200

    add html with code for user google to see if seo is ok

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, ''));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
 	res.render('index');
 });
 
