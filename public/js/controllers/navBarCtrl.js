87824c633c858ba8dae127702dc034b5c8000a86
elissaios <elissaios13@hotmail.com>
2018-02-19 16:08:36 +0200

    make dropdown menu

--- public/js/controllers/navBarCtrl.js
+++ public/js/controllers/navBarCtrl.js
@@ -3,4 +3,8 @@ app.controller('navBar', function($scope, $http) {
 	$scope.cart = JSON.parse(localStorage.cart);
 
 	$http.get(`${url}/categories/navigation-bar`).then(({ data }) => ($scope.navBar = data));
+	$http.get(`${url}/categories/otherProducts-dropdown`).then(({ data }) => {
+		$scope.otherProductsDropdown = data
+		console.log($scope.otherProductsDropdown)
+	})
 });
